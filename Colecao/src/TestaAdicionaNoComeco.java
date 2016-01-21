4f5d1907749712f5b5c8c951722c3d0b5fd95462
k11-26 <k11-26@k34>
2016-01-21 15:32:01 -0200

    Atualiza

---
+++ Colecao/src/TestaAdicionaNoComeco.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TestaAdicionaNoComeco {
+	public static void main(String[] args) {
+		ArrayList arrayList = new ArrayList();
+		LinkedList linkedList = new LinkedList();
+
+		long tempo = TestaAdicionaNoComeco.adicionaNoComeco(arrayList);
+		System.out.println("Comeco ArrayList : " + tempo + "ms");
+
+		tempo = TestaAdicionaNoComeco.adicionaNoComeco(linkedList);
+		System.out.println("Comeco LinkedList : " + tempo + "ms");
+	}
+
+	public static long adicionaNoComeco(List lista) {
+		long inicio = System.currentTimeMillis();
+		int size = 100000;
+		for (int i = 0; i < size; i++) {
+			lista.add(0, i);
+		}
+		long fim = System.currentTimeMillis();
+		return fim - inicio;
+	}
+
+}
