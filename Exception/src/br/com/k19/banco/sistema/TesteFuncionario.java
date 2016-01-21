3f5eaacbc3f7e5f7a6819ab1dd5d2292f10cca89
Ale DP <dpsantos@gmail.com>
2016-01-21 11:50:33 -0200

    Atualizacao

--- Exception/src/br/com/k19/banco/sistema/TesteFuncionario.java
+++ Exception/src/br/com/k19/banco/sistema/TesteFuncionario.java
@@ -4,10 +4,9 @@ public class TesteFuncionario {
 	public static void main(String[] args) {
 		Funcionario f = new Funcionario();
 		try {
-			f.setSalario(-1000);
+			f.aumentaSalario(-1000);
 		} catch (IllegalArgumentException e) {
 			System.out.println("Houve erro");
 		}
-
 	}
 }
