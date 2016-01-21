f733e61c347afe9e910a035ba30b61c4bee72183
Ale DP <dosantos@gmail>
2016-01-21 13:21:34 -0200

    Atualizado

--- String/src/br/com/k19/string/TestaImutabilidade.java
+++ String/src/br/com/k19/string/TestaImutabilidade.java
@@ -2,6 +2,10 @@ package br.com.k19.string;
 
 public class TestaImutabilidade {
 	public static void main(String[] args) {
+		String nome = "Chico Ação Ferreira";
 
+		System.out.println(nome);
+
+		System.out.println(nome.toUpperCase());
 	}
 }
